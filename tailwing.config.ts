d117a7b915fafd8cf39688ab6abc62965d1101d9
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2025-11-28 14:18:23 +0200

    initalized project

---
+++ tailwing.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./ui/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
